e5dc569afd03a730d6167b7a08ed850736fb142c
nguyenquy42 <nguyenquy26012014@gmail.com>
2021-04-01 19:29:04 +0700

    up css

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import logo from './logo-header.png';
+
 
 
 import './style.css';
@@ -10,40 +12,73 @@ class Header extends Component {
     return (
       <div className="header">
 
-        <div className="footer--top">
+        <div className="header--top">
           <div className="container">
             <div className="row">
               <div className="col-md-6">
-                <Link to={'/'} className="Logo__Top">Sách Củ</Link>
+                <Link to={'/'} className="Logo__Top">
+                  <img
+                    className="logo-img"
+                    src={logo}
+                    alt="img"
+                  />
+                </Link>
               </div>
               <div className="col-md-6">
-                <ul className="m-0">
-                  <li>Email: ntquy97@gmail.com</li>
-                </ul>
+                <div className="box--CoAu">
+                  <ul className="m-0">
+                    <li>Email: ntquy97@gmail.com</li>
+                    <li>Phone: 086580****</li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link to={'/'} className="navbar-brand">My app</Link>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to={'/'} className="nav-link">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link to={'/san-pham'} className="nav-link">Sản Phẩm</Link>
-              </li>
-              <li className="nav-item">
-                <Link to={'/login'} className="nav-link">Đăng Nhập</Link>
-              </li>
-              <li className="nav-item">
-                <Link to={'/sign-up'} className="nav-link">Đăng Ký</Link>
-              </li>
-            </ul>
+        <div className="bg-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8">
+                <nav className="navbar navbar-expand-lg navbar-light ">
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                      <li className="nav-item">
+                        <Link to={'/'} className="nav-link">Home</Link>
+                      </li>
+                      <li className="nav-item dropdown">
+                        <Link to={'/#'} className="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Sản Phẩm</Link>
+                        <ul className="dropdown-menu">
+                          <Link to={'/#'} className="dropdown-item">Sản Phẩm 1</Link>
+                          <Link to={'/#'} className="dropdown-item">Sản Phẩm 2</Link>
+                          <Link to={'/#'} className="dropdown-item">Sản Phẩm 3</Link>
+                        </ul>
+                      </li>
+                      <li className="nav-item">
+                        <Link to={'/bai-viet'} className="nav-link">Blog Sách</Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to={'/lien-he'} className="nav-link">Liên Hệ</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+              <div className="col-lg-4">
+                <div className="box--CoAu">
+                  <ul className="m-0">
+                    <li className="nav-item">
+                      <Link to={'/login'} className="nav-link">Đăng Nhập</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to={'/sign-up'} className="nav-link">Đăng Ký</Link>
+                    </li>
+                    <li>Hi: User</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
-        </nav>
+        </div>
       </div>
     )
   }
