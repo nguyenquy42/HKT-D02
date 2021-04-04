dd9176149957d6d9976c6fc51a118d8d10758db3
nguyenquy42 <nguyenquy26012014@gmail.com>
2021-04-04 21:18:18 +0700

    up list category in home

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -1,31 +1,24 @@
 import React, { useState } from 'react';
 
+
 import './style.css';
 
 const List = (props) => {
   const [newPost, setnewPost] = useState('');
-  // const { post: { id, author, content, isClicked, comments }, onClickPost } = props;
+  const { post: { id, img_url, content, isClicked, price }, onClickPost } = props;
 
-
-  const renderPost = () => {
-    return (
-      <div className="col-lg-4">
-        <div className="Post">
-          <div className="Post__img">
-            <img className="img" src={'assets/img/product-1.jpg'} />
-          </div>
-          <div className="Post__content">
-            <h5>Những Quy Luật Của Bản Chất Con Người</h5>
-            <p>187.700 đ</p>
-          </div>
-        </div>
-      </div>
-    )
-  }
 
   return (
-    <div className="row">
-      {renderPost()}
+    <div className="col-lg-3">
+      <div className="Post">
+        <div className="Post__img">
+          <img className="img" src={img_url} />
+        </div>
+        <div className="Post__content">
+          <h5>{content}</h5>
+          <p>{price} đ</p>
+        </div>
+      </div>
     </div>
   )
 
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,24 +1,95 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 
 import './style.css';
 import Owldemo1 from '../../components/Owldemo1';
 import List from '../../components/List';
 
-class Home extends Component {
 
-  render() {
-    return (
-      <div>
-        <Owldemo1></Owldemo1>
+
+const postList = [
+  {
+    id: uuidv4(),
+    author: "Công Hiếu",
+    content: "Quy luật bản chất con người.",
+    isClicked: false,
+    img_url: "assets/img/product-1.jpg",
+    price: 270000
+  },
+  {
+    id: uuidv4(),
+    author: "Ngọc Hiệp",
+    content: "Đọc vị bất kỳ ai.",
+    isClicked: false,
+    img_url: "assets/img/product-2.jpg",
+    price: 180000
+  },
+  {
+    id: uuidv4(),
+    author: "Minh Quý",
+    content: "Muôn kiếp thân sinh.",
+    isClicked: false,
+    img_url: "assets/img/product-3.jpg",
+    price: 310000
+  },
+];
+
+
+const Home = () => {
+  const [posts, setPosts] = useState(postList)
+
+  const handleClickPost = (postId) => {
+    const newPosts = posts.map((post) => {
+      if (post.id === postId) return { ...post, isClicked: !post.isClicked };
+      return post;
+    });
+
+    setPosts(newPosts)
+  };
+
+  return (
+    <div>
+      <Owldemo1></Owldemo1>
+
+      <section className="">
         <div className="container">
-          <List></List>
+          <div className="row">
+            <div className="col-12">
+              <h2>Phổ biến</h2>
+            </div>
+
+            {posts.map((post) => (
+              <List
+                key={post.id}
+                post={post}
+                onClickPost={handleClickPost}
+              />
+            ))}
+          </div>
         </div>
-        <h1>thân</h1>
-      </div>
+      </section>
+      
+      <section className="">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h2>Bán chạy</h2>
+            </div>
 
-    )
-  }
+            {posts.map((post) => (
+              <List
+                key={post.id}
+                post={post}
+                onClickPost={handleClickPost}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
 
+    </div>
+  )
 }
 
 export default Home;
